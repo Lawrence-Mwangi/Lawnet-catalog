2f83ec76c03158de52053ba6cf533d8ffa7ac8c1
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2025-08-17 00:37:32 +0300

    Add the contact and the about page and they style to make the website more interactive with the client

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,13 +5,51 @@
 </script>
 
 <template>
-  <Navbar/>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <!-- <h1>{{ $company.name }}</h1> -->
-  </div>
+    <Navbar/>
+  
+    <div class="about_container main_container">
+        <h2>About Us</h2>
 
-  <Footer/>
+        <div class="about_history">
+            <div class="about">
+                <div class="image">
+                    <img src="../assets/image/about_us.jpg" alt="Guitar image">
+                </div>
+                <div class="about_infor">
+                    <p>At Lawnet Decor, we design and craft unique epoxy resin products, from elegant wall clocks and tables to 
+                        stylish chessboards and keychains. Each piece blends durability with modern design, making it perfect
+                        for homes, offices, or gifting.
+                    </p>
+                    
+                    <p>Our goal is to create products that are both functional and visually striking, 
+                        bringing creativity and elegance into everyday spaces.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="our_goals">
+            <h2>Our Goals</h2>
+
+            <div class="goals_infor">
+                <div class="mission">
+                    <h4>Our Mission</h4>
+                    <p>To craft unique, high-quality epoxy resin products that combine functionality with 
+                        artistic design, delighting customers and enhancing everyday spaces.
+                    </p>
+                </div>
+
+                <div class="vision">
+                    <h4>Our Vision</h4>
+                    <p>To be recognized as a leading creator of innovative and stylish resin decor products, 
+                        inspiring beauty and creativity in every home and office.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <Footer/>
 </template>
 
 
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,61 @@
 <script setup>
     import Navbar from '@/components/NavbarComponents.vue'
     import Footer from '@/components/FooterComponents.vue'
+    import { ref } from "vue"
+
+    const name = ref("")
+    const email = ref("")
+    const contact = ref("")
+    const message = ref("")
+   
+    const whatsappNumber = "254799509242"
+
+    const sendWhatsApp = () => {
+    if (!name.value || !email.value || !contact.value || !message.value) {
+        alert("Please fill in all fields before sending!")
+        return
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailPattern.test(email.value)) {
+        alert("Please enter a valid email address!")
+        return
+    }
+
+    const text = `Hello, my name is ${name.value}. Contact: ${contact.value}. Message: ${message.value}`
+    const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`
+    window.open(url, "_blank")
+    }
 </script>
 
 <template>
     <Navbar/>
+
+    <div class="contact_container main_container">
+        <h2>Reach us</h2>
+
+        <div class="contact_infor">
+            <div class="contact_content">
+                <p class="contact_item">
+                    <img src="../assets/image/gmail.png" alt="email icon" width="25px" height="25px"> 
+                    {{ $company.email }}
+                </p>
+                
+                <p class="contact_item">
+                    <img src="../assets/image/phone.png" alt="email icon" width="25px" height="25px">
+                     {{ $company.phone }}
+                </p>
+            </div>
+
+            <div class="contact_form">
+                <input v-model="name" placeholder="Example: Lawnet Decor">
+                <input v-model="email" placeholder="Example: infor@lawnetdecor.com">
+                <input v-model="contact" placeholder="Example: 0799509242">
+                <textarea v-model="message" placeholder="Example: How much is a Chessboard"></textarea>
+                <button @click="sendWhatsApp">Send via WhatsApp</button>
+            </div>
+        </div>
+    </div>
     
-    <p>This is the service Page</p>
     <Footer/>
 </template>
