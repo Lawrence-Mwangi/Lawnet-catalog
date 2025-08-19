86299e0e8e22ef3b0980f41cf45a20a28d6f8aec
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2025-08-19 16:26:40 +0300

    Update the images on the product file to make sure they same size and background

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -12,42 +12,42 @@
         <div class="gallery_content">
             <div class="gallery_item">
                 <div class="gallery_image">
-                    <img src="/public/assets/images/chessboard.jpg" alt="Gallery Image">
+                    <img src="/public/assets/images/chessboard1.jpeg" alt="Gallery Image">
                 </div>
                 <h4>Title: <span>Chessboard Set</span></h4>
             </div>
 
             <div class="gallery_item">
                 <div class="gallery_image">
-                    <img src="/public/assets/images/clock1.jpeg" alt="Gallery Image">  
+                    <img src="/public/assets/images/clock4.jpeg" alt="Gallery Image">  
                 </div>
                 <h4>Title: <span>Wall Clock</span></h4>
             </div>
 
             <div class="gallery_item">
                 <div class="gallery_image">
-                    <img src="/public/assets/images/bookset1.jpeg" alt="Gallery Image">
+                    <img src="/public/assets/images/notebook.jpeg" alt="Gallery Image">
                 </div>
                 <h4>Title: <span>Note Book Set</span></h4>
             </div>
 
             <div class="gallery_item">
                 <div class="gallery_image">
-                    <img src="/public/assets/images/phone.jpg" alt="Gallery Image"> 
+                    <img src="/public/assets/images/phoneholder.jpeg" alt="Gallery Image"> 
                 </div>
                 <h4>Title: <span>Phone Holder</span></h4>
             </div>
 
             <div class="gallery_item">
                 <div class="gallery_image">
-                    <img src="/public/assets/images/lamp.jpg" alt="Gallery Image">                   
+                    <img src="/public/assets/images/lamp.jpeg" alt="Gallery Image">                   
                 </div>
                 <h4>Title: <span>Bed Lamp</span></h4>
             </div>
 
              <div class="gallery_item">
                 <div class="gallery_image">
-                    <img src="/public/logo.jpg" alt="Gallery Image">  
+                    <img src="/public/assets/images/chessset.jpeg" alt="Gallery Image">  
                 </div>
                 <h4>Title: <span>Chess Set</span></h4>
             </div>
